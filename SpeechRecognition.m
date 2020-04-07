d57f5bac6885c00587b47b58a3f1a9723fa34a22
Gabriel martinez fresneda <gamar1995@gmail.com>
2020-04-07 13:12:53 +0200

    Delete SpeechRecognition.m

--- SpeechRecognition.m
+++
@@ -1,3 +0,0 @@
-datafolder = 'C:\Users\Gabriel\Documents\Universidad\MUIT\Asignaturas\Semestre 4\Deep Learning\Proyecto\';
-ads = audioDatastore(datafolder,'IncludeSubfolders',true,'FileExtensions','.wav','LabelSource','foldernames');
-ads0 = copy(ads);
